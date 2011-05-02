212fc3476b76319c810be79056bc49f43d582078
Pete X. Graham <petegraham1@gmail.com>
2011-05-02 21:43:20 +0100

    user login working now

--- application/controllers/dress.php
+++ application/controllers/dress.php
@@ -2,16 +2,54 @@
 
 class Dress extends CI_Controller {
 	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+		//$this->load->library('form_validation');
+		//$this->load->library('security');
+		$this->load->library('tank_auth');
+		$this->lang->load('tank_auth');
+	}
+		
 	public function index()
 	{
+		#TODO: move this to the constructor? shall we use $this->data instead? Maybe we could set header there
+		$userName = '';
+		if ($this->tank_auth->is_logged_in()) {
+			$loggedIn = TRUE;
+			$userName = $this->tank_auth->get_username();
+		}else{
+			$loggedIn = FALSE;
+		}
+		
 		#TODO? refactor to call this getAll and use routes to set this as the index?
 		$this->load->model('Dressmodel', '', TRUE);
 		$data['dresses'] = $this->Dressmodel->get_all();
-		//prd($dresses);
 		$data['page_title'] = 'Dress Catalogue';
-		//$this->load->view('header', $data);
-		//$this->load->view('menu');
+		#TODO? can we assign all of these together?
+		$data['logged_in'] = $loggedIn;
+		$data['username'] = $userName;
+		
+		$this->load->view('header', $data);
 		$this->load->view('all_dresses', $data);
-		//$this->load->view('footer');
+		$this->load->view('footer');
+	}
+	
+	public function view($id)
+	{
+		#TODO: could use Codeigniter built in url matching from routes here
+		if(preg_match( '/^[0-9]+$/', $id) == FALSE){
+			redirect(base_url());
+		}
+		
+		#TODO: lookup dress in DB here
+		$this->load->model('Dressmodel', '', TRUE);
+		$data['dresses'] = $this->Dressmodel->get_all();
+		
+		#TODO: get user info here (can we put in constructor?)
+		$this->load->view('header', $data);
+		$this->load->view('dress_detail', $data);
+		$this->load->view('footer');
 	}
 }
---
+++ application/views/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $page_title; ?> > Wish Wear Want</title>
+
+<style type="text/css">
+
+body {
+ background-color: #fff;
+ margin: 40px;
+ font-family: Lucida Grande, Verdana, Sans-serif;
+ font-size: 14px;
+ color: #4F5155;
+}
+
+a {
+ color: #003399;
+ background-color: transparent;
+ font-weight: normal;
+}
+
+h1 {
+ color: #444;
+ background-color: transparent;
+ border-bottom: 1px solid #D0D0D0;
+ font-size: 16px;
+ font-weight: bold;
+ margin: 24px 0 2px 0;
+ padding: 5px 0 6px 0;
+}
+
+code {
+ font-family: Monaco, Verdana, Sans-serif;
+ font-size: 12px;
+ background-color: #f9f9f9;
+ border: 1px solid #D0D0D0;
+ color: #002166;
+ display: block;
+ margin: 14px 0 14px 0;
+ padding: 12px 10px 12px 10px;
+}
+
+</style>
+</head>
+<body>
+
+<div id="user_status">
+<?php if($logged_in): ?>
+	<p><?php echo $username ?> <a href="/index.php/auth/logout">logout</a></p>
+<?php else: ?>
+	<p><a href="/index.php/auth/login">login</a></p>
+<?php endif; ?>
+</div>
+
+<h1><?php echo $page_title; ?></h1>
